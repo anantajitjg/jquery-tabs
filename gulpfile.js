e903ab778be3d1961a328244133daf28be74b4dd
anantajitjg <anantajitjg2010@gmail.com>
2017-10-02 21:13:02 +0530

    New gulp tasks added

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,44 @@
-var gulp = require("gulp");
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
-gulp.task('init', function(){
+gulp.task('init', function () {
     return console.log("Gulp tasks for jQuery Tabs started!");
 });
 
-gulp.task('default', ['init']);
+// compile sass file and minify css file
+gulp.task('sass', function () {
+    return gulp.src('src/scss/jquery.tabs.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('dist/'))
+        .pipe(cleanCSS())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('dist/'));
+});
+
+// minify js
+gulp.task('uglify', function () {
+    return gulp.src('src/js/jquery.tabs.js')
+        .pipe(uglify({
+            output: {
+                comments: /^!/
+            }
+        }))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('dist/'));
+});
+
+// watch files
+gulp.task('watch', function () {
+    gulp.watch('src/scss/*.scss', ['sass']);
+    gulp.watch('src/js/jquery.tabs.js', ['uglify'])
+});
+
+// default tasks
+gulp.task('default', ['init', 'sass', 'uglify']);
